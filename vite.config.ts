f5d410404ea88f094053a3afca034bd98c17a82b
RomelSikdar <romels500@gmail.com.com>
2023-12-27 21:07:40 +0530

    Landing page package added and vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/editor/schema",
   server: {
-    port: 3002,
+    port: 3003,
     proxy: {
       "/admin": {
-        target: "http://localhost:3000/",
+        target: "http://localhost:3001/",
         changeOrigin: true,
       },
       "/editor/flow": {
-        target: "http://localhost:3001/",
+        target: "http://localhost:3002/",
+        changeOrigin: true,
+      },
+      "/auth": {
+        target: "http://localhost:3000/",
         changeOrigin: true,
       },
     },
