bc664499700bede33e5bd8a96be94fa3d8492b8d
RomelSikdar <romels500@gmail.com.com>
2024-02-20 19:22:35 +0530

    code refractered

--- src/service/connections/deleteConnection.service.tsx
+++ src/service/connections/deleteConnection.service.tsx
@@ -1,19 +1,10 @@
 import axios from "../../utils/axios/axios";
 
-const deleteConnectionService = (ConnectionName: string): Promise<any> => {
-  return new Promise<any>(async (resolve, reject) => {
-    try {
-      resolve(
-        (
-          await axios.delete("schema/connection", {
-            data: { ConnectionName: ConnectionName },
-          })
-        ).data
-      );
-    } catch (err) {
-      reject(err);
-    }
-  });
+const deleteConnectionService = async (
+  ConnectionName: string
+): Promise<any> => {
+  return (await axios.delete("schema/connection", { data: { ConnectionName } }))
+    .data;
 };
 
 export default deleteConnectionService;
