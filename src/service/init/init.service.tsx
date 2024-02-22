89e750fc6e6ec48e1783e885387dc7ded63914f0
RomelSikdar <romels500@gmail.com.com>
2024-02-22 16:28:37 +0530

    false Redirect fix

--- src/service/init/init.service.tsx
+++ src/service/init/init.service.tsx
@@ -1,45 +1,41 @@
 import _ from "lodash";
 import axios from "../../utils/axios/axios";
-import wrapPromise from "../../utils/suspense/wrapPromise";
+import { ApiResponse } from "@eco-flow/types";
 
-const promise = () =>
-  new Promise<any>(async (resolve, reject) => {
-    try {
-      let result = {
-        isAuth: false,
-        isNew: false,
-        isLoggedIn: false,
-        userID: undefined,
-      };
-      const res = (await axios.get("init/status")).data;
-      result.isAuth = res.isAuth;
+const promise = async (): Promise<any> => {
+  let result = {
+    isAuth: false,
+    isNew: false,
+    isLoggedIn: false,
+    userID: undefined,
+  };
+  const res = (await axios.get("init/status")).data;
+  result.isAuth = res.isAuth;
 
-      if (!res.isAuth) {
-        if (_.has(res, "getAccessToken"))
-          axios.defaults.headers.common[
-            "Authorization"
-          ] = `Bearer ${res.getAccessToken}`;
-        result.isLoggedIn = true;
-        result.userID = res.userID;
-      }
+  if (!res.isAuth) {
+    if (_.has(res, "getAccessToken"))
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${res.getAccessToken}`;
+    result.isLoggedIn = true;
+    result.userID = res.userID;
+  }
 
-      if (res.isAuth && res.isNewClient) result.isNew = true;
+  if (res.isAuth && res.isNewClient) result.isNew = true;
 
-      if (res.isAuth && !res.isNewClient)
-        try {
-          if ((await axios.get("auth/users/isAuthenticated")).data.success) {
-            result.isLoggedIn = true;
-            //To-Do: get user information;
-          }
-        } catch {
-          result.isLoggedIn = false;
-        }
+  if (res.isAuth && !res.isNewClient) {
+    const ress: ApiResponse = (await axios.get("auth/users/isAuthenticated"))
+      .data;
 
-      setTimeout(() => resolve(result), 200);
-    } catch (err) {
-      reject({ error: true, code: 504, payload: err });
+    if (ress.success) {
+      result.isLoggedIn = true;
+      result.userID = ress.payload._id;
     }
-  });
+  }
 
-export default wrapPromise(promise());
+  return await new Promise<any>((resolve) =>
+    setTimeout(() => resolve(result), 200)
+  );
+};
+
 export const initService = async () => await promise();
